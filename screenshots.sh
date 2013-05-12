8471814fa094eba92a806902e62a28c8ed6b9ec2
Petter Blomberg <github@petterblomberg.se>
2013-05-13 00:19:41 +0200

    Time to grab some screenshots.

---
+++ screenshots.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /Users/sanfred/timetracker
+DATE=`date +%Y-%m-%d`
+TIME=`date +%Y-%m-%d--%H-%M-%S`
+mkdir -p $DATE
+cd $DATE
+screencapture -x ${TIME}_1.png ${TIME}_2.png ${TIME}_3.png ${TIME}_4.png ${TIME}_5.png ${TIME}_6.png ${TIME}_7.png ${TIME}_8.png ${TIME}_9.png ${TIME}_10.png
