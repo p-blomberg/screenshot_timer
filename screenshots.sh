8a993b92b80e80a056123a064a3d7cd557e0334a
Petter Blomberg <github@petterblomberg.se>
2013-05-13 01:45:47 +0200

    Merging screens with imagemagick. Now hard-coded for two screens.

--- screenshots.sh
+++ screenshots.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
+
+# Change this to wherever you want the screenshots
 cd /Users/sanfred/timetracker
+
 touch last_run
 DATE=`date +%Y-%m-%d`
 TIME=`date +%Y-%m-%d--%H-%M-%S`
+
+# Remove this if you don't want a new directory every day
 mkdir -p $DATE
 cd $DATE
-screencapture -x ${TIME}_1.png ${TIME}_2.png ${TIME}_3.png ${TIME}_4.png ${TIME}_5.png ${TIME}_6.png ${TIME}_7.png ${TIME}_8.png ${TIME}_9.png ${TIME}_10.png
+
+# Take the screenshot
+screencapture -x ${TIME}_1.png ${TIME}_2.png
+
+# Merge the screens using imagemagick
+/usr/local/bin/montage -geometry +0+0 ${TIME}_2.png ${TIME}_1.png ${TIME}.png && rm ${TIME}_1.png ${TIME}_2.png
+# Notice the order of the screen on the montage line, 
+# because I want the second screen to the left of the first.
+# If you don't have imagemagick, "brew install imagemagick"
+# or find yourself with separate images for every screen.
